2e1c59c4a56ae59c4f199446dced85800960d96b
phucnguyen <phucnguyendigital2003@gmail.com>
2025-05-13 21:45:39 +0700

    Setup and configuration for user service module

---
+++ user/src/main/java/com/ecommerce/user/service/UserService.java
@@ -0,0 +1,49 @@
+package com.ecommerce.user.service;
+
+import com.ecommerce.user.dto.UserRequest;
+import com.ecommerce.user.dto.UserResponse;
+import com.ecommerce.user.model.User;
+import com.ecommerce.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class UserService {
+    private final UserRepository userRepository;
+    private final ModelMapper modelMapper;
+
+    public List<UserResponse> fetchAllUsers() {
+        return userRepository.findAll().stream().map(user -> {
+            return modelMapper.map(user, UserResponse.class);
+        }).collect(Collectors.toList());
+    }
+
+    public void addUser(UserRequest userRequest) {
+        User user = modelMapper.map(userRequest, User.class);
+        userRepository.save(user);
+    }
+
+    public Optional<UserResponse> fetchUser(Long id) {
+        return userRepository.findById(id).map(user -> {
+            return modelMapper.map(user, UserResponse.class);
+        });
+    }
+
+    public boolean updateUser(Long id, UserRequest userRequest) {
+        return userRepository.findById(id)
+                .map(existingUser -> {
+                    existingUser.setFirstName(userRequest.getFirstName());
+                    existingUser.setLastName(userRequest.getLastName());
+                    userRepository.save(existingUser);
+                    return true;
+                }).orElse(false);
+    }
+}

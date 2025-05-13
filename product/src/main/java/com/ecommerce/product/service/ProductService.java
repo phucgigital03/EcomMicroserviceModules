b793bca2df7424554b304bca133bfd5bf5308fc7
phucnguyen <phucnguyendigital2003@gmail.com>
2025-05-13 22:27:14 +0700

    Setup and configuration for product service module

---
+++ product/src/main/java/com/ecommerce/product/service/ProductService.java
@@ -0,0 +1,65 @@
+package com.ecommerce.product.service;
+
+import com.ecommerce.product.dto.ProductRequest;
+import com.ecommerce.product.dto.ProductResponse;
+import com.ecommerce.product.model.Product;
+import com.ecommerce.product.repository.ProductRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class ProductService {
+    private final ProductRepository productRepository;
+    private final ModelMapper modelMapper;
+
+    public ProductResponse createProduct(ProductRequest productRequest) {
+        Product product;
+        product = modelMapper.map(productRequest, Product.class);
+        Product savedProduct = productRepository.save(product);
+        return modelMapper.map(savedProduct, ProductResponse.class);
+    }
+
+
+    public Optional<ProductResponse> updateProduct(Long id, ProductRequest productRequest) {
+        return productRepository.findById(id).map(existingProduct ->{
+            existingProduct.setName(productRequest.getName());
+            existingProduct.setDescription(productRequest.getDescription());
+            existingProduct.setPrice(productRequest.getPrice());
+            existingProduct.setStockQuantity(productRequest.getStockQuantity());
+            existingProduct.setCategory(productRequest.getCategory());
+            existingProduct.setImageUrl(productRequest.getImageUrl());
+            Product updatedProduct = productRepository.save(existingProduct);
+            return modelMapper.map(updatedProduct, ProductResponse.class);
+        });
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        List<Product> products = productRepository.findByActiveTrue();
+        return products.stream()
+                .map(product -> modelMapper.map(product, ProductResponse.class))
+                .toList();
+    }
+
+
+    public boolean deleteProduct(Long id) {
+        return productRepository.findById(id).map(existingProduct -> {
+            existingProduct.setActive(false);
+            productRepository.save(existingProduct);
+            return true;
+        }).orElse(false);
+    }
+
+    public List<ProductResponse> searchProducts(String keyword) {
+        return productRepository.searchProducts(keyword).stream()
+                .map(product -> modelMapper.map(product, ProductResponse.class))
+                .collect(Collectors.toList());
+    }
+}

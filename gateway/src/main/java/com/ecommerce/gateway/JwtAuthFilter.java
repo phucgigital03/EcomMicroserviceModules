e00d63fc5c1526061351c0084936b1ee036bd435
phucnguyen <phucnguyendigital2003@gmail.com>
2025-05-20 14:49:35 +0700

    setup Eureka Discovery server via API gateway

--- gateway/src/main/java/com/ecommerce/gateway/JwtAuthFilter.java
+++ gateway/src/main/java/com/ecommerce/gateway/JwtAuthFilter.java
@@ -4,14 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
 
-@Component
+//@Component
 public class JwtAuthFilter implements WebFilter {
     private static final Logger logger = LoggerFactory.getLogger(JwtAuthFilter.class);
 
